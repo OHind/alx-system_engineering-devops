2a3f9fad94911cc540a11b075d6365c1d0028eb5
OHind <ouchani.hind@gmail.com>
2024-01-02 22:20:42 +0100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Display TODO list of a given employee using REST API"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+        url = 'https://jsonplaceholder.typicode.com/'
+        employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+        todos = requests.get(url + "todos", params={
+            "userId": sys.argv[1]}).json()
+        completed = []
+        for t in todos:
+            if t.get("completed") is True:
+                completed.append(t.get("title"))
+        print("Employee {} is done with tasks({}/{}):".format(
+            employee.get("name"), len(completed), len(todos)))
+        for task in completed:
+            print("\t {}".format(task))
